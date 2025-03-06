219c772e7fb8007ab4493e0f84009c60d138c035
Mathias Chevez <davidchevez90@gmail.com>
2025-03-06 06:19:59 -0700

    simplify

--- src/api/messages.ts
+++ src/api/messages.ts
@@ -4,8 +4,6 @@ import { store } from "../redux/store";
 export async function fetchMessages() {
   const messages = await fetch(`${import.meta.env.VITE_API_URL}/messages`, {
     method: "POST",
-    headers: new Headers({ "Content-Type": "application/json" }),
-    redirect: "follow",
   }).then(res => res.json());
 
   store.dispatch(addMessages(messages.map((m: { _id: string, message: string }) => m.message)));
